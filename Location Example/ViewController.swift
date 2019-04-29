625c35af7096e417c774f1f4e25be2a83e7cd670
Apple <apple@Apples-Mac-mini.local>
2019-04-29 15:00:17 +0530

    Initial Commit

---
+++ Location Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location Example
+//
+//  Created by Apple on 29/04/19.
+//  Copyright © 2019 GoMobileLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
